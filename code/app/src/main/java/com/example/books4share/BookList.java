fcfe8e9f44f34893ebd3aa93d64553e312699358
dazhi-z <dazhi@ualberta.ca>
2020-11-07 07:22:23 +0800

    message

--- code/app/src/main/java/com/example/books4share/BookList.java
+++ code/app/src/main/java/com/example/books4share/BookList.java
@@ -5,9 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Filter;
-import android.widget.Filterable;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,98 +12,36 @@ import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
 
-public class BookList extends ArrayAdapter<Book> implements Filterable {
-    private ArrayList<Book> books;
+public class BookList extends ArrayAdapter<Books> {
+    private ArrayList<Books> books;
     private Context context;
-    private ArrayList<Book> filteredBooks;
 
-    public BookList(Context context, ArrayList<Book> books) {
+    public BookList(Context context, ArrayList<Books> books) {
         super(context,0,books);
         this.books = books;
-        this.filteredBooks = books;
         this.context = context;
     }
 
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view = convertView;
 
-        if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.mybooks_content, parent, false);
+        if (view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.book_item,parent,false);
         }
-        Book book = filteredBooks.get(position);
+
+        Books book = books.get(position);
+
+        TextView bookTitle = view.findViewById(R.id.book_title);
+        TextView bookAuthor = view.findViewById(R.id.book_author);
+        TextView bookISBN = view.findViewById(R.id.book_ISBN);
+        TextView bookStatus = view.findViewById(R.id.book_status);
 
 
-        TextView bookName = view.findViewById(R.id.textView_book_title);
-        TextView bookAuthor = view.findViewById(R.id.textView_book_author);
-        TextView bookIsbn = view.findViewById(R.id.textView_book_isbn);
-        TextView bookStatus = view.findViewById(R.id.textView_book_status);
-        TextView bookBorrower = view.findViewById(R.id.textView_book_burrower);
-        Button editButton = view.findViewById(R.id.button_edit_book);
-
-
-        bookName.setText(book.getTitle());
+        bookTitle.setText(book.getTitle());
         bookAuthor.setText(book.getAuthor());
-        bookIsbn.setText(book.getIsbn());
-        bookStatus.setText(book.getCurrentStatus().name());
-        bookBorrower.setText("Burrower: In progress");
-
-        // Testing
-        editButton.setVisibility(View.GONE);
-
-
+        bookISBN.setText(book.getISBN());
+        //bookStatus.setText(book.getStatus());
 
         return view;
-    }
-
-    public int getCount() {
-        return filteredBooks.size();
-    }
-
-    public Book getItem(int position) {
-        return filteredBooks.get(position);
-    }
-
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public Filter getFilter() {
-        Filter filter = new Filter() {
-            @Override
-            protected FilterResults performFiltering(CharSequence constraint) {
-                FilterResults results = new FilterResults();
-                ArrayList<Book> tempList = new ArrayList<>();
-                String c = constraint.toString();
-
-                if (c.equals("ALL")) {
-                    tempList = books;
-                } else {
-                    for (Book book : books) {
-                        if (book.getCurrentStatus() == Book.Status.valueOf(c)) {
-                            tempList.add(book);
-                        }
-                    }
-                }
-
-                results.values = tempList;
-                results.count = tempList.size();
-
-                return results;
-            }
-
-            @Override
-            protected void publishResults(CharSequence constraint, FilterResults results) {
-                filteredBooks = (ArrayList<Book>) results.values;
-                if (results.count > 0) {
-                    notifyDataSetChanged();
-                } else {
-                    notifyDataSetInvalidated();
-                }
-
-            }
-        };
-
-        return filter;
     }
 }
--- code/app/src/main/java/com/example/books4share/MainActivity.java
+++ code/app/src/main/java/com/example/books4share/MainActivity.java
@@ -2,69 +2,84 @@ package com.example.books4share;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String TAG = "MainActivity";
+    ListView bookList;
+    Button btnSearch;
+    ArrayAdapter<Books> bookAdapter;
+    ArrayList<Books> bookDataList;
 
-    TextView ProjectName;
-    TextView Team;
-    TextView Sentence;
-    Button MainLogin;
-    Button MainSignup;
+    String[] titles = {"Harry Porter", "The Republic", "The Odyssey", "Lord of the Flies"};
+    String[] authors = {"J.K. Rowling", "Plato", "Homer", "William"};
+    String[] ISBNs = {"1", "2", "3", "4"};
+    String[] status = {"Requested", "Requested", "Unrequested", "Requested"};
 
-    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button newButton = findViewById(R.id.new_button);
 
-        newButton.setOnClickListener(new View.OnClickListener() {
+        //Write all books into an ArrayList
+        bookList=findViewById(R.id.bookList);
+        bookDataList=new ArrayList<>();
+        for(int i=0;i<titles.length;i++){
+            bookDataList.add(new Books(titles[i], authors[i], ISBNs[i]));
+        }
+
+        bookAdapter=new BookList(this, bookDataList);
+        bookList.setAdapter(bookAdapter);
+
+        btnSearch = findViewById(R.id.btn_search);
+        //Once the button is clicked, do search
+        btnSearch.setOnClickListener(new View.OnClickListener() {
             @Override
+            //Detect if the search button is clicked
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                startActivity(intent);
+                //Get the content of search box
+                EditText editText = (EditText) findViewById(R.id.edit_text);
+                String inputText = editText.getText().toString();
+                //Delete the book which name is not the input
+                if (!inputText.equals("")){
+                    for(int i=0;i<bookDataList.size();i++) {
+                        if (!bookDataList.get(i).getTitle().equals(inputText)) {
+                            bookDataList.remove(i);
+                            i--;
+                            //Indicate some book(s) is deleted
+                            setTitle("1");
+                        }
+                    }
+                }
+                //Return all books if there is no input
+                else{
+                    bookDataList.clear();
+                    for(int i=0;i<titles.length;i++){
+                        bookDataList.add(new Books(titles[i],authors[i],ISBNs[i]));
+                    }
+                }
+                bookAdapter.notifyDataSetChanged();
             }
         });
-        ProjectName = findViewById(R.id.project_name);
-        Team = findViewById(R.id.team_name);
-        Sentence = findViewById(R.id.statement);
-        MainLogin = findViewById(R.id.login_button);
-        MainSignup = findViewById(R.id.signup_button);
-
-
-        db = FirebaseFirestore.getInstance();
-
-        MainLogin.setOnClickListener(new View.OnClickListener() {
+        //When a book item is clicked, send a request
+        //Unfinished, I will implement this next time
+        bookList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-
-                Intent LoginAct = new Intent(MainActivity.this, Login.class);
-                startActivityForResult(LoginAct, 1);
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), "A mock request is sent!", Toast.LENGTH_SHORT).show();
             }
         });
-
-        MainSignup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent SignupAct = new Intent(MainActivity.this, Signup.class);
-                startActivity(SignupAct);
-            }
-        });
-
-
+        //Indicate the list is refreshed
+        setTitle("4");
     }
 }
